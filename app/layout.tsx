a127aa829cb70e7dbd373ee429608d728a906749
ajayess1998 <132552669+ajayess1998@users.noreply.github.com>
2026-05-06 17:02:41 +0530

    updated some css changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,34 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import { Lato } from "next/font/google";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+import { Lato, Poppins } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Cardora",
   description: "Find Your Dream Ride",
 };
 
-
-
 const lato = Lato({
   subsets: ["latin"],
   weight: ["300", "400", "700", "900"],
   variable: "--font-lato",
 });
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], 
+  variable: "--font-poppins",
+});
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html
       lang="en"
-      // className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-       className={`${lato.variable} h-full antialiased`}
+      className={`${lato.variable} ${poppins.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Providers>{children}</Providers>
