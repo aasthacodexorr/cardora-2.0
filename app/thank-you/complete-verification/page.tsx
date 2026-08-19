ecd6bec952c72f730771ff17cf11bb3f3198f8c8
AasthaCodexorr <aastha.codexorr@gmail.com>
2026-08-19 16:11:56 +0530

    UI of complete verification page

--- app/thank-you/complete-verification/page.tsx
+++ app/thank-you/complete-verification/page.tsx
@@ -8,7 +8,7 @@ export default function CarLoanLanding() {
       <Header />
       <main className="lg:mt-20 font-sans text-gray-800 bg-white ">
         {/* 1. Confirmation Banner Section */}
-        <section className="py-20 pb-44 text-gray-900 bg-background-light">
+        <section className="my-44 text-gray-900">
           <div className="max-w-7xl mx-auto px-4 text-center">
             {/* Heading */}
             <h2 className="text-2xl md:text-[30px] font-bold">
@@ -16,12 +16,12 @@ export default function CarLoanLanding() {
             </h2>
 
             {/* Subheading */}
-            <p className="text-lg md:text-xl font-bold mb-16">
+            <p className="text-lg md:text-xl font-bold my-5">
               We respond within 2 Business Hours.
             </p>
 
             {/* Buttons Container */}
-            <div className="flex flex-col sm:flex-row justify-between items-center mt-20 gap-4 sm:gap-12 md:gap-24 w-full px-4">
+            <div className="flex flex-col sm:flex-row justify-between items-center mt-20 gap-4 sm:gap-12 md:gap-24 w-full px-44">
               <Link href={"/inventory"} className="whitespace-nowrap cursor-pointer text-white text-base py-3 px-6 rounded-xl transition duration-200 shadow-sm hover:opacity-90 bg-brand">
                 View In-Stock Inventory
               </Link>
