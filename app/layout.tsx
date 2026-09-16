b68181dabbee6a4cec02a74c12a4915b3b4e551c
AasthaCodexorr <aastha.codexorr@gmail.com>
2026-09-16 13:46:40 +0530

    fixed top space

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,7 +96,7 @@ export default async function RootLayout({
       <body className="min-h-full flex flex-col">
         <Providers config={appConfig}>
           <Header />
-          <div className="flex-1 flex flex-col min-h-screen mt-10">
+          <div className="flex-1 flex flex-col min-h-screen mt-10 lg:mt-0">
             {children}
           </div>
           <Footer />
